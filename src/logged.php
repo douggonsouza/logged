95e3b55db98073dd4c3ef71e7803eaf63cfada62
Douglas G. de Souza <douggonsouza@gmail.com>
2022-08-16 07:27:32 -0300

    Insere alertas

--- src/logged.php
+++ src/logged.php
@@ -2,45 +2,85 @@
 
 namespace douggonsouza\logged;
 
+use douggonsouza\propertys\propertys;
 use douggonsouza\propertys\propertysInterface;
 
 abstract class logged
 {
+    const NAME_SESSION = 'userlog';
+    
+    /**
+     * in - Inclui infos no usuário da sessão
+     *
+     * @param  mixed $info
+     * @return void
+     */
     static public function in(propertysInterface $info)
     {
+        // limpa o usuário da sessão
+        if(isset($_SESSION[self::NAME_SESSION])){
+            unset($_SESSION[self::NAME_SESSION]);
+        }
+
         // colocar dados na sessão
-        $_SESSION['user']['info']     = $dtUser;
-        $_SESSION['user']['licenses'] = $licenses;
+        $_SESSION[self::NAME_SESSION] = $info;
 
         return true;
     }
-
+    
+    /**
+     * out - Limpa infos do usuário na sessão
+     *
+     * @return void
+     */
     static public function out()
     {
-        // desconecta e limpa o usuário da sessão
-        if(isset($_SESSION['user']))
-            unset($_SESSION['user']);
+        // limpa o usuário da sessão
+        if(isset($_SESSION[self::NAME_SESSION])){
+            unset($_SESSION[self::NAME_SESSION]);
+        }
 
         return true;
     }
-
+    
+    /**
+     * get - Expõe as infos do usuário na sessão
+     *
+     * @return void
+     */
     static public function get()
     {
-        if(isset($_SESSION['user']) && !empty($_SESSION['user']))
-            return $_SESSION['user'];
+        if(isset($_SESSION[self::NAME_SESSION]) && !empty($_SESSION[self::NAME_SESSION]))
+            return $_SESSION[self::NAME_SESSION];
 
         return null;
     }
-
-    static public function alter()
+    
+    /**
+     * info - Forma info propertys
+     *
+     * @param  mixed $info
+     * @return void
+     */
+    static public function info(array $info)
     {
+        if(!isset($info) || empty($info)){
+            return null;
+        }
 
+        return new propertys($info);
     }
-
-    static public function isLogged()
+    
+    /**
+     * isLogged - Valida se usuário está logado na sessão
+     *
+     * @return void
+     */
+    static public function is()
     {
-        if(isset($_SESSION['user']) && isset($_SESSION['user']['info']['user_id']))
+        if(isset($_SESSION[self::NAME_SESSION]) && !empty($_SESSION[self::NAME_SESSION])){
             return true;
+        }
 
         return false;
     }
